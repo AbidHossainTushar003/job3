fcc09fa2d18bfebd063d23b83af52ff839d0aecf
sharif <muhammadsharifulislam33@gmail.com>
2026-08-28 18:11:11 +0600

    001

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -8,6 +9,9 @@ android {
         version = release(37)
     }
 
+    buildFeatures {
+        viewBinding=true
+    }
     defaultConfig {
         applicationId = "com.example.job3"
         minSdk = 24
@@ -36,6 +40,12 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.firestore)
+    implementation(libs.googleid)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
